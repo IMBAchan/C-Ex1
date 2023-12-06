4c9d84e1d88755a5dd217bf245ce53a8b60fd908
IMBAchan <114655325+IMBAchan@users.noreply.github.com>
2023-12-06 17:21:20 +0200

    Update lub8.cpp

--- lub8.cpp
+++ lub8.cpp
@@ -47,35 +47,4 @@ int main() {
     for (int i = 0; i < arraySize; ++i) {
         cout << arr[i] << ' ';
     }
-
-    int arr2[arraySize];
-    cout << endl;
-
-    srand(time(NULL));
-    for (int i = 0; i < arraySize; ++i) {
-        arr2[i] = rand() % 10 - 6;
-        cout << arr2[i] << " ";
-    }
-
-    int maxNum = 0;
-    int maxIndex = 0;
-    for (int i = 0; i < arraySize; ++i) {
-        if (maxNum < arr2[i]) {
-            maxNum = arr2[i];
-            maxIndex = i;
-        }
-    }
-
-    //task2
-    for (int i = 0; i < maxIndex; ++i) {
-        if (arr2[i] < 0) {
-            arr2[i] = 0;
-        }
-    }
-
-    cout << endl;
-
-    for (int i = 0; i < arraySize; ++i) {
-        cout << arr2[i] << ' ';
-    }
 }
